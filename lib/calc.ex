64fb05db960b54f616c3735ac333a774ce2c7c1a
cottonblue <cottonblue713@gmail.com>
2020-12-20 22:31:29 +0900

    add test in inertial coord.

--- lib/calc.ex
+++ lib/calc.ex
@@ -4,7 +4,7 @@ defmodule Calc do
 
   defp init_values do
     %{
-      pi: 3.14,
+      pi: 3.14159,
       gravity: 9.8,
       radius: 6_400_000.0,
       earth_angular_velocity: 729211.59,
@@ -12,8 +12,16 @@ defmodule Calc do
     }
   end
 
+  def get_period() do
+    # initial coord
+    %{pi: pi, gravity: g, length: l} = init_values()
+    2 * pi * pow(l/g, 0.5)
+#    |> Float.round(10)
+  end
+
   # TODO: add dialyzer
   def get_period(0.0) do
+    # at equator
     :infinity 
   end
 
@@ -30,9 +38,17 @@ defmodule Calc do
     [a1, b1, a2, b2] = [1.0, 1.0, 1.0, 1.0]
     %{gravity: g, length: l} = init_values()
 
-    x = a1 * sin(pow(g/l, 0.5) * t) + b1 * cos(pow(g/l, 0.5) * t)
-    y = a2 * sin(pow(g/l, 0.5) * t) + b2 * cos(pow(g/l, 0.5) * t) 
+    # Constants for period (by gravity, length)
+    c = 2.05359140961
+    tp = t - c
+    x = a1 * sin(pow(g/l, 0.5) * tp) + b1 * cos(pow(g/l, 0.5) * tp)
+    y = a2 * sin(pow(g/l, 0.5) * tp) + b2 * cos(pow(g/l, 0.5) * tp)
     %{x: x, y: y}
+  end
+
+  def get_position(:non_inertial, 0.0, t) do
+    # at equator
+    get_position(:inertial, 0.0, t)
   end
 
   def get_position(:non_inertial, l, t) do
--- test/calc_test.exs
+++ test/calc_test.exs
@@ -4,10 +4,11 @@ defmodule CalcTest do
 
   describe "get_period/1" do
     test "at north pole" do
-      assert 8.612e-6 == Calc.get_period(pi() / 2.0)
+      assert 8.6164e-6 == Calc.get_period(pi() / 2.0)
     end
 
     test "at northern hemisphere" do
+      assert 1.21854e-5 == Calc.get_period(pi() / 4.0)
     end
 
     test "at equator" do
@@ -15,30 +16,39 @@ defmodule CalcTest do
     end
 
     test "at southern hemisphere" do
+      assert -1.21854e-5 == Calc.get_period(-(pi() / 4.0))
     end
    
     test "at south pole" do
-      assert -8.612e-6 == Calc.get_period(-(pi() / 2.0))
+      assert -8.6164e-6 == Calc.get_period(-(pi() / 2.0))
     end
   end
 
   describe "get_postion in inertial coord" do
     test "0s" do
-      assert %{x: 1.0, y: 1.0} = Calc.get_position(:inertial, 0, 0)
+      %{x: x, y: y} = Calc.get_position(:inertial, 0, 0)
+      assert %{x: 0.0, y: 0.0} == %{x: x |> Float.round(3), y: y |> Float.round(3)} 
     end
 
     test "1/4 period" do
+      p = Calc.get_period() / 4.0
+      %{x: x, y: y} = Calc.get_position(:inertial, 0, p)
+      assert %{x: 1.414, y: 1.414} == %{x: x |> Float.round(3), y: y |> Float.round(3)} 
     end
  
     test "1/2 period" do
     end
 
     test "1 period" do
+      p = Calc.get_period()
+      %{x: x, y: y} = Calc.get_position(:inertial, 0, p)
+      assert %{x: 0.0, y: 0.0} == %{x: x |> Float.round(3), y: y |> Float.round(3)} 
     end
   end
 
   describe "get_postion in non-inertial coord" do
   end
 
-  defp pi(), do: 3.14
+  # TODO: make constants
+  defp pi(), do: 3.14159
 end
