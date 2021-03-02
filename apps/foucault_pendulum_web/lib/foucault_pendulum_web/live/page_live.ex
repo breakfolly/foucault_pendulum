5f3b28e215f08c223c8373d30c65532a778bf5c4
cottonblue <cottonblue713@gmail.com>
2021-03-02 19:21:37 +0900

    add pole_width/height to draw in html

--- apps/foucault_pendulum_web/lib/foucault_pendulum_web/live/page_live.ex
+++ apps/foucault_pendulum_web/lib/foucault_pendulum_web/live/page_live.ex
@@ -5,20 +5,32 @@ defmodule FoucaultPendulumWeb.PageLive do
 
   @impl true
   def mount(_params, _session, socket) do
-    time = 0
-    degree = 90
-    radian = Calc.degree_to_radian(degree)
+    # from css
+    earth_radius = 150
 
+    degree = 45
+    radian = Math.deg2rad(degree)
+    pole_width = (earth_radius * Math.cos(radian)) |> Float.ceil() |> max(3)
+    pole_height = (earth_radius * Math.sin(radian)) |> Float.ceil() |> max(3)
+
+    time = 0.0
     inertial_period = Calc.get_period()
     period = Calc.get_period(radian)
+
     %{x: x, y: y} = Calc.get_position(radian, time)
 
     s =
       socket
-      |> assign(:time, time)
+      # Latitude Data
       |> assign(:degree, degree)
+      |> assign(:radian, radian)
+      |> assign(:pole_width, pole_width)
+      |> assign(:pole_height, pole_height)
+      # Current Data
+      |> assign(:time, time)
       |> assign(:period, period)
       |> assign(:inertial_period, inertial_period)
+      # Pantheon Data
       |> assign(:x, x)
       |> assign(:y, y)
 
@@ -32,7 +44,7 @@ defmodule FoucaultPendulumWeb.PageLive do
     degree = socket.assigns.degree
 
     %{x: x, y: y} =
-      Calc.degree_to_radian(degree)
+      Math.deg2rad(degree)
       |> Calc.get_position(time)
 
     s =
